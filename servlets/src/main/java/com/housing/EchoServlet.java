9e93299e6b04ab098354a97223f62130eea4b302
Student <student@localhost>
2012-12-12 11:24:17 +0100

    removed to folder

--- servlets/src/main/java/com/housing/EchoServlet.java
+++ servlets/src/main/java/com/housing/EchoServlet.java
@@ -2,13 +2,11 @@ package com.housing;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 
 @WebServlet(name = "EchoServlet", urlPatterns = { "/Echo" }, description = "retrieves some test table from database")
@@ -27,12 +25,12 @@ public class EchoServlet  extends HttpServlet {
                 noData(response);
                 return;
             }
-            //final String jsonStr = Database.instance.getCodeTable(currSql, name, param, null);
-            final String jsonStr = "{ \"name\":\"echo\", \"aaData\": [\"prodam\",\"kupim\",\"zamenjam\"]}";
+            final String jsonStr = Database.instance.vrniVseSekcije();
+            final String jsonAll = "{ \"name\":\"echo\", \"aaData\": " + jsonStr + " }";
             log.info("EchoServlet finished processing");
             response.setContentType("application/json;charset=UTF-8");
             final PrintWriter out = response.getWriter();
-            out.print(jsonStr);
+            out.print(jsonAll);
             out.flush();
             return;
         } catch (Exception ex) {
